0ca87cffe4af4b792e0d41eef6dba9730884d97f
BANDARU4 <74387859+BANDARU4@users.noreply.github.com>
2022-08-02 18:29:26 +0530

    Update ClassThr.java
    added code in existing class

--- Bdd_Proj2/src/main/java/QA/Bdd_Proj2/ClassThr.java
+++ Bdd_Proj2/src/main/java/QA/Bdd_Proj2/ClassThr.java
@@ -8,5 +8,6 @@ public static void main(String args[])
 	System.out.println("Going to Eclipse");
 	System.out.println("Going to github");
 	System.out.println("Bye");
+	System.out.println("HAPPY");
 }
 }
